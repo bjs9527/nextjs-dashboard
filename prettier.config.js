b452d7c51f9fa11dca1f8b7dc1125f0f2453134c
bjs9527 <bjs9527@outlook.com>
2024-02-27 20:42:55 +0800

    chapter11

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
-const styleguide = require('@vercel/style-guide/prettier');
+const styleguide = require ('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  // singleQuote: true, // 使用单引号
+  // printWidth: 115,
+  // proseWrap: 'always',
+  // semi: false, // 不加分号
+  // trailingComma: 'none', // 结尾处不加逗号
+  // htmlWhitespaceSensitivity: 'ignore', // 忽略'>'下落问题
 };
